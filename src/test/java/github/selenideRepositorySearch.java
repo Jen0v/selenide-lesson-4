755a1037a377b69eb64a18f7c49b89ec4b05168c
Anna <jenova.anya@yandex.ru>
2024-09-24 22:53:59 +0300

    added second lesson

---
+++ src/test/java/github/selenideRepositorySearch.java
@@ -0,0 +1,29 @@
+package github;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class selenideRepositorySearch {
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop() {
+
+        // открыть главную страницу
+        open("https://github.com/");
+        // ввести в поле поиска selenide и нажать enter
+        $("[placeholder='Search or jump to...']").setValue("selenide").pressEnter();
+        // кликнуть на первый репозиторий из списка найденых
+        $$("ul.repo-list li").first().$("a").click();
+        // проверка: заголовок selenide/selenide
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+
+
+        // ARRANGE
+        // ACT
+        // ACT
+        // (ASSERT)
+        // ACT
+        // ASSERT
+    }
+}
